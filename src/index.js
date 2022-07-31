5ce91d75ecd78493b37f71532150cbec7e917799
3V3RYONE <beleswarprasad@gmail.com>
2022-07-31 09:39:56 +0530

    keep index.js unchanged

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import App from './App';
 import Home from './pages/home';
 import Events from './pages/events/events';
 import LearnLinux from './pages/learnLinux/learnLinux';
+import EscapeHunt from './pages/escapeHunt/escapeHunt';
 // import UserForm from './pages/recruitment_2022/recruitment';
-import RescueTheTux from './pages/rescueTheTux/home';
 
 const pages = [
-    /*{
+    {
         title: 'Home',
         link: '/',
         component: <Home />
@@ -28,11 +28,11 @@ const pages = [
         title: 'Learn Linux',
         link: '/learn-linux',
         component: <LearnLinux />
-    },*/
+    },
     {
-        title: 'Rescue Tux',
-        link: '/',
-        component: <RescueTheTux />
+        title: 'Escape Hunt',
+        link: '/escape-hunt',
+        component: <EscapeHunt />
     }
     //, {
     //     title: 'Recruitment 2022',
@@ -42,27 +42,31 @@ const pages = [
 ];
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <App pages={pages}>
-              <Outlet />
-            </App>
-          }
-        >
-          {pages.map((page, index) =>
-            page.link === '/' ? (
-              <Route key={index} index element={page.component} />
-            ) : (
-              <Route key={index} path={page.link} element={page.component} />
-            )
-          )}
-        </Route>
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <App pages={pages}>
+                            <Outlet />
+                        </App>
+                    }
+                >
+                    {pages.map((page, index) =>
+                        page.link === '/' ? (
+                            <Route key={index} index element={page.component} />
+                        ) : (
+                            <Route
+                                key={index}
+                                path={page.link}
+                                element={page.component}
+                            />
+                        )
+                    )}
+                </Route>
+            </Routes>
+        </HashRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
